7085a9b3fb70e67dad8c80a53c643938b7b0b9d3
TLaw763 <diego.aqu@outlook.com>
2022-03-11 12:40:54 +0200

    Imported VueJS template, made styling adjustments

---
+++ Enigmic/src/App.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header class="container">
+      <nav class="nav">
+        <RouterLink class="col-md-4 text-center my-auto" to="/portfolio">Portfolio</RouterLink>
+        <RouterLink class="col-md-4 text-center" to="/"><img alt="Enigmic logo" class="logo" src="@/assets/Enigmic.Logo.Long.02.png" /></RouterLink>
+        <RouterLink class="col-md-4 text-center my-auto" to="/contact">Contact</RouterLink>
+      </nav>
+  </header>
+
+  <RouterView />
+</template>
+
+<style>
+#app {
+  background-color: #261C2D;
+  color: #7A7A7A;
+  font-family:'Courier New', Courier, monospace;
+  font-weight: 500;
+}
+
+.logo {
+  max-width: 100%;
+}
+
+.nav {
+  width: 100%;
+  font-size: 2rem;
+}
+
+.nav a {
+  text-decoration: none;
+  color: #7A7A7A;;
+  font-weight: 700;
+}
+
+</style>
---
+++ Enigmic/src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <main class="home">
+      <h1>This is the home page</h1>
+  </main>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .home {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
